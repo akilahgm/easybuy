72d5a8e0159b8207fd5e392e3c51e19d851ac31e
akilahgm <akilahgm@gmail.com>
2020-09-12 15:57:14 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // await app.listen(process.env.PORT || 1024);
-  await app.listen(80);
+  await app.listen(1024);
 }
 bootstrap();
